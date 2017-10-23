e73462dd9894235c79398e369430cdf2f9e2f136
mhamill2 <hamill.33@osu.edu>
2017-10-22 20:44:55 -0400

    Added user model class. Added database creation to LoginActivity. Checked for existing user after login. Changed email to user name and added validations.

--- app/src/main/java/database/OsuTourDbSchema/DatabaseHelper.java
+++ app/src/main/java/database/OsuTourDbSchema/DatabaseHelper.java
@@ -1,8 +1,11 @@
 package database.OsuTourDbSchema;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import osu.mobile_apps.ohiostatetourchallenge.User;
 
 /*
  * This class is used to handle database creation and updating.
@@ -18,32 +21,35 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate (SQLiteDatabase db) {
-        db.execSQL("create table " + OsuTourDbSchema.UserTable.NAME + "(" + " _id int primary key autoincrement, " +
+        db.execSQL("create table " + OsuTourDbSchema.UserTable.NAME + "(" + " _id integer primary key autoincrement, " +
                 OsuTourDbSchema.UserTable.Cols.USER_NAME + " varchar(20) not null, " +
                 OsuTourDbSchema.UserTable.Cols.PASSWORD + " varchar(20) not null)"
         );
-        db.execSQL("create table " + OsuTourDbSchema.LocationTable.NAME+ "(" + " _id int primary key autoincrement, " +
+        db.execSQL("create table " + OsuTourDbSchema.LocationTable.NAME+ "(" + " _id integer primary key autoincrement, " +
                 OsuTourDbSchema.LocationTable.Cols.NAME + " varchar(50) not null, " +
                 OsuTourDbSchema.LocationTable.Cols.LATITUDE + " decimal(9, 6) not null, " +
                 OsuTourDbSchema.LocationTable.Cols.LONGITUDE + " decimal(9, 6) not null, " +
                 OsuTourDbSchema.LocationTable.Cols.HISTORY + " text)"
         );
-        db.execSQL("create table " + OsuTourDbSchema.QuestionTable.NAME+ "(" + " _id int primary key autoincrement, " +
+        db.execSQL("INSERT INTO "+ OsuTourDbSchema.LocationTable.NAME +" VALUES(1, 'Ohio Union', 39.997687, -83.008590, 'The Ohio Union serves as an activity center for students of The Ohio State University. When the Union was established in 1910, it was the first student union at a public university. The Ohio Union provides facilities for student activities, organizations/events, and campus / community interaction. Many student services and programs are housed in the union, along with dining and recreational facilities. It also serves as the home base for the D-Tix program, which provides discounted tickets to students.')");
+
+        db.execSQL("create table " + OsuTourDbSchema.QuestionTable.NAME+ "(" + " _id integer primary key autoincrement, " +
                 OsuTourDbSchema.QuestionTable.Cols.LOCATION_ID + " int not null, " +
                 OsuTourDbSchema.QuestionTable.Cols.TEXT + " text not null)"
         );
-        db.execSQL("create table " + OsuTourDbSchema.AnswerTable.NAME+ "(" + " _id int primary key autoincrement, " +
+
+        db.execSQL("create table " + OsuTourDbSchema.AnswerTable.NAME+ "(" + " _id integer primary key autoincrement, " +
                 OsuTourDbSchema.AnswerTable.Cols.TEXT + " text not null)"
         );
-        db.execSQL("create table " + OsuTourDbSchema.PlayerLocationCompletedTable.NAME + "(" + " _id int primary key autoincrement, " +
+        db.execSQL("create table " + OsuTourDbSchema.PlayerLocationCompletedTable.NAME + "(" + " _id integer primary key autoincrement, " +
                 OsuTourDbSchema.PlayerLocationCompletedTable.Cols.USER_ID + " int not null, " +
                 OsuTourDbSchema.PlayerLocationCompletedTable.Cols.LOCATION_ID + " int not null)"
         );
-        db.execSQL("create table " + OsuTourDbSchema.LocationQuestionTable.NAME+ "(" + " _id int primary key autoincrement, " +
+        db.execSQL("create table " + OsuTourDbSchema.LocationQuestionTable.NAME+ "(" + " _id integer primary key autoincrement, " +
                 OsuTourDbSchema.LocationQuestionTable.Cols.LOCATION_ID + " int not null, " +
                 OsuTourDbSchema.LocationQuestionTable.Cols.QUESTION_ID + " int not null)"
         );
-        db.execSQL("create table " + OsuTourDbSchema.QuestionAnswerTable.NAME+ "(" + " _id int primary key autoincrement, " +
+        db.execSQL("create table " + OsuTourDbSchema.QuestionAnswerTable.NAME+ "(" + " _id integer primary key autoincrement, " +
                 OsuTourDbSchema.QuestionAnswerTable.Cols.QUESTION_ID + " int not null, " +
                 OsuTourDbSchema.QuestionAnswerTable.Cols.ANSWER_ID + " int not null, " +
                 OsuTourDbSchema.QuestionAnswerTable.Cols.IS_CORRECT + " int not null)"
@@ -53,6 +59,25 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public User getUser(String userName) {
+        User user = new User();
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {OsuTourDbSchema.UserTable.Cols.USER_NAME, OsuTourDbSchema.UserTable.Cols.PASSWORD};
+        Cursor cursor =
+                db.query(OsuTourDbSchema.UserTable.NAME,
+                        columns,
+                        OsuTourDbSchema.UserTable.Cols.USER_NAME + " = ?",
+                        new String[] {String.valueOf(userName)},
+                        null, null, null, null);
+        if (cursor != null && cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            user.setUserName(cursor.getString(0));
+            user.setPassword(cursor.getString(1));
+        }
+
+        return user;
     }
 
 }
--- app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/LoginActivity.java
+++ app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/LoginActivity.java
@@ -3,8 +3,11 @@ package osu.mobile_apps.ohiostatetourchallenge;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -34,12 +37,20 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import database.OsuTourDbSchema.DatabaseHelper;
+import database.OsuTourDbSchema.OsuTourDbSchema;
+
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    private Context mContext;
+    private SQLiteDatabase mDatabaseWrite;
+    private SQLiteDatabase mDatabaseRead;
+    private DatabaseHelper mDatabaseHelper = new DatabaseHelper(this);
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -68,6 +79,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("REQUIRED","OnCreate() Executed");
         super.onCreate(savedInstanceState);
+        mDatabaseWrite = new DatabaseHelper(this).getWritableDatabase();
+        mDatabaseRead = new DatabaseHelper(this).getReadableDatabase();
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
@@ -91,8 +104,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
-                Intent intent = new Intent(LoginActivity.this, ListActivity.class);
-                startActivity(intent);
             }
         });
 
@@ -202,7 +213,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
@@ -217,7 +228,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mEmailView.setError(getString(R.string.error_invalid_email));
             focusView = mEmailView;
             cancel = true;
+        } else {
+            User user = mDatabaseHelper.getUser(email);
+            if (user.getPassword() != null) {
+                if (!user.getPassword().equals(password)) {
+                    mPasswordView.setError(getString(R.string.incorrect_password));
+                    focusView = mPasswordView;
+                    cancel = true;
+                }
+            }
         }
+
 
         if (cancel) {
             // There was an error; don't attempt login and focus the first
@@ -229,18 +250,20 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent intent = new Intent(LoginActivity.this, ListActivity.class);
+            startActivity(intent);
         }
         Log.d("TESTING", "After login");
     }
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.length() > 5;
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() > 6;
     }
 
     /**
@@ -342,6 +365,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         private final String mEmail;
         private final String mPassword;
 
+
         UserLoginTask(String email, String password) {
             mEmail = email;
             mPassword = password;
@@ -367,6 +391,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
+            ContentValues values = new ContentValues();
+            values.put(OsuTourDbSchema.UserTable.Cols.USER_NAME, mEmail);
+            values.put(OsuTourDbSchema.UserTable.Cols.PASSWORD, mPassword);
+            long newRowId = mDatabaseWrite.insert(OsuTourDbSchema.UserTable.NAME, null, values);
             return true;
         }
 
