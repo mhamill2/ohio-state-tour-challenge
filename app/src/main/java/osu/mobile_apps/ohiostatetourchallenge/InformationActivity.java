fc05eb35030c90065cfdfd2a6d0f60543bc83fab
Trevor Rambacher <trev@rambacher.com>
2017-10-30 11:46:02 -0400

    Added question activity and outlined some additional things with commented out code

--- app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/InformationActivity.java
+++ app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/InformationActivity.java
@@ -30,53 +30,64 @@ public class InformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("TESTING", "InformationActivity onCreate()");
-
-        //Check for location permission
-        mLocationPermissionGranted = ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION);
         setContentView(R.layout.activity_information);
 
-        TextView TV = (TextView) findViewById(R.id.textView);
-        ImageView image = findViewById(R.id.image);
+        //TODO Check if location is locked for user
+        //if (location locked) {
+        //Check for location permission
+            mLocationPermissionGranted = ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION);
 
-        //TODO check if at location
-        try {
-            if (mLocationPermissionGranted == PackageManager.PERMISSION_GRANTED) {
-                getDeviceLocation();
+
+
+            //TODO get distance to location, check if close enough
+            try {
+                if (mLocationPermissionGranted == PackageManager.PERMISSION_GRANTED) {
+                    getDeviceLocation();
+                }
+            } catch (SecurityException e) {
+                Log.e("Exception: %s", e.getMessage());
             }
-        }catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
-        }
 
-        Location location = (Location) getIntent().getSerializableExtra("Location");
+            Location location = (Location) getIntent().getSerializableExtra("Location");
 
-        SimpleLocation.Point myCoords = new SimpleLocation.Point(deviceLocation.getLatitude(), deviceLocation.getLongitude());
-        SimpleLocation.Point targetCoords = new SimpleLocation.Point(location.getLatitude(),location.getLongitude());
+            SimpleLocation.Point myCoords = new SimpleLocation.Point(deviceLocation.getLatitude(), deviceLocation.getLongitude());
+            SimpleLocation.Point targetCoords = new SimpleLocation.Point(location.getLatitude(), location.getLongitude());
 
-        double distance = information.calculateDistance(myCoords,targetCoords);
-        /*
-        if(distance > 100){
+            double distance = information.calculateDistance(myCoords, targetCoords);
+            Log.d("TESTING", "Distance: " + distance);
+
+                /* TODO
+                //meters?
+                if(distance > 100){
                     //Say too far away
+                    TV = (TextView) findViewById(R.id.description);
+                    if (TV != null){
+                        TV.setText("Please move closer to location");
+                    }
 
-        }else{
+                }else{
                     //Show challenge question
-
-        }
-        */
-        Log.d("TESTING", "Distance: " + distance);
-
-
-        //Display everything
-        String locationImage = location.getName().toLowerCase().replace(" ", "").replace("(", "").replace(")", "");
-        int resourceId = this.getResources().getIdentifier(locationImage, "drawable", "osu.mobile_apps.ohiostatetourchallenge");
-        image.setImageResource(resourceId);
-        if (TV != null){
-            TV.setText(location.getName());
-        }
-        TV = (TextView) findViewById(R.id.description);
-        if (TV != null){
-            TV.setText(location.getDescription());
-        }
-        Log.d("TESTING", "Data = " + location.getName());
+                    Intent intent = new Intent(ListActivity.this, QuestiontActivity.class);
+                    intent.putExtra("Location", location.getName());
+                    startActivity(intent);
+                }
+                */
+        //}else{
+            //Display everything, since location is unlocked and viewable from anywhere
+            TextView TV = (TextView) findViewById(R.id.textView);
+            ImageView image = findViewById(R.id.image);
+            String locationImage = location.getName().toLowerCase().replace(" ", "").replace("(", "").replace(")", "");
+            int resourceId = this.getResources().getIdentifier(locationImage, "drawable", "osu.mobile_apps.ohiostatetourchallenge");
+            image.setImageResource(resourceId);
+            if (TV != null){
+                TV.setText(location.getName());
+            }
+            TV = (TextView) findViewById(R.id.description);
+            if (TV != null){
+                TV.setText(location.getDescription());
+            }
+            Log.d("TESTING", "Data = " + location.getName());
+        //}
     }
 }
---
+++ app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/QuestionActivity.java
@@ -0,0 +1,57 @@
+package osu.mobile_apps.ohiostatetourchallenge;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+        Location location = (Location) getIntent().getSerializableExtra("Location");
+        TextView TV = (TextView) findViewById(R.id.textView);
+        ImageView image = findViewById(R.id.image);
+
+        //Display everything, since location is unlocked and viewable from anywhere
+        String locationImage = location.getName().toLowerCase().replace(" ", "").replace("(", "").replace(")", "");
+        int resourceId = this.getResources().getIdentifier(locationImage, "drawable", "osu.mobile_apps.ohiostatetourchallenge");
+        image.setImageResource(resourceId);
+        if (TV != null){
+            TV.setText(location.getName());
+        }
+        TV = (TextView) findViewById(R.id.question);
+        if (TV != null){
+            //TODO get location question
+            TV.setText("Question goes here");
+        }
+
+        //TODO set answer buttons text
+        Button B1 = (Button) findViewById(R.id.Button1);
+        B1.setText("Set1");
+        Button B2 = (Button) findViewById(R.id.Button2);
+        B2.setText("Set2");
+        Button B3 = (Button) findViewById(R.id.Button3);
+        B3.setText("Set3");
+        Button B4 = (Button) findViewById(R.id.Button4);
+        B4.setText("Set4");
+
+    }
+
+    public void onClick(View v){
+        //TODO check if answer is correct
+        Button selected = findViewById(v.getId());
+        String answer = (String) selected.getText();
+
+        //TODO If correct unlock location and go to location information
+
+        //TODO If incorrect display incorrect toast
+
+    }
+}
