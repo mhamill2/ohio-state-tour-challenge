3eadf27ca3e81160a41ceb72b555e5ac864702d5
unknown <hamill.33@osu.edu>
2017-10-29 21:02:52 -0400

    Added and changed app launcher icon

--- app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/myAdpater.java
+++ app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/myAdpater.java
@@ -1,10 +1,8 @@
 package osu.mobile_apps.ohiostatetourchallenge;
 
 import android.content.Context;
-import android.print.PrintDocumentAdapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
