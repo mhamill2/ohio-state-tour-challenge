775522a3cc7f2ea3795d52b293fb40396bcab337
Trevor Rambacher <trev@rambacher.com>
2017-10-30 10:59:02 -0400

    Selecting a location should calculate the distance between the device and that location

--- app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/InformationActivity.java
+++ app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/InformationActivity.java
@@ -1,12 +1,29 @@
 package osu.mobile_apps.ohiostatetourchallenge;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
+import im.delight.android.location.SimpleLocation;
+
 public class InformationActivity extends AppCompatActivity {
+    private int mLocationPermissionGranted;
+    private GoogleMap mMap;
+    private SimpleLocation deviceLocation;
+    private SimpleLocation targetLocation;
+    private SimpleLocation information;
+
+    public void getDeviceLocation() {
+        Context context = this;
+        deviceLocation = new SimpleLocation(context, false);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +35,35 @@ public class InformationActivity extends AppCompatActivity {
 
         TextView TV = (TextView) findViewById(R.id.textView);
         ImageView image = findViewById(R.id.image);
+
+        //TODO check if at location
+        try {
+            if (mLocationPermissionGranted == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                getDeviceLocation();
+            }
+        }catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+
+            // Add a marker in current location
+            LatLng myLocation = new LatLng(deviceLocation.getLatitude(), deviceLocation.getLongitude());
+            LatLng targetCoords = new LatLng(location.getLatitude(),location.getLongitude());
+
+            double distance = information.calculateDistance(myLocation.latitude,myLocation.longitude, targetCoords.latitude, targetCoords.longitude);
+
+            Log.d("TESTING", "Distance: " + distance);
+
         String locationImage = location.getName().toLowerCase().replace(" ", "").replace("(", "").replace(")", "");
         int resourceId = this.getResources().getIdentifier(locationImage, "drawable", "osu.mobile_apps.ohiostatetourchallenge");
         image.setImageResource(resourceId);
         if (TV != null){
             TV.setText(location.getName());
-        }else{
-            Log.d("TESTING","TV is null");
         }
         TV = (TextView) findViewById(R.id.description);
         if (TV != null){
             TV.setText(location.getDescription());
-        }else{
-            Log.d("TESTING","TV is null");
         }
         Log.d("TESTING", "Data = " + location.getName());
     }
