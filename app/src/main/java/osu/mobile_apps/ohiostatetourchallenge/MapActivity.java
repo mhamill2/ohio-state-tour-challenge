8a5387ec5ba6e26745b9c7424973054036f45fff
Vyyom-Kelkar <kelkar.20@osu.edu>
2017-11-03 04:10:16 -0400

    added TODOs to mapactivity

--- app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/MapActivity.java
+++ app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/MapActivity.java
@@ -37,16 +37,10 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mapFragment.getMapAsync(this);
     }
 
+    // TODO - Markers for all locations
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // TODO - update/remove methods for markers
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
