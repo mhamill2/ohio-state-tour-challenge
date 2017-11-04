fd03919a6d812654101c2a17b38c8571248b53b9
unknown <hamill.33@osu.edu>
2017-11-04 10:45:49 -0400

    Changed distance to 50

--- app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/InformationActivity.java
+++ app/src/main/java/osu/mobile_apps/ohiostatetourchallenge/InformationActivity.java
@@ -60,7 +60,7 @@ public class InformationActivity extends AppCompatActivity {
 
         double distance = deviceLocation.calculateDistance(myCoords, targetCoords);
         Log.d("TESTING", "Distance: " + distance + " meters");
-            if (distance > 1000) {
+            if (distance > 50) {
                 //Say too far away
                 TV = (TextView) findViewById(R.id.description);
                 if (TV != null) {
